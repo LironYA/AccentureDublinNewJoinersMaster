15515785e7c9c810ca167c0c4ac08d3af98ba480
LironYA <33940316+LironYA@users.noreply.github.com>
2019-12-06 08:50:55 +0000

    Fab
    Fab menu

--- app/src/main/java/com/example/android/accenturenewjoiners/Activities/Benefits/BenefitsMain.java
+++ app/src/main/java/com/example/android/accenturenewjoiners/Activities/Benefits/BenefitsMain.java
@@ -5,9 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
